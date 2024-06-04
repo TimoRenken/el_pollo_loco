04bc26d4b2a73b7b19cc24b7284f61e7c336f438
TimoRenken <timorenken@outlook.de>
2024-06-04 10:28:43 +0200

    add images to objects

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,13 @@
 class MovableObject {
-    x;
-    y;
+    x = 120
+    y = 400
     img;
+
+    
+    loadImage(path){           // loadImage('img/test.png')
+        this.img= new Image(); // this.img = document.getElementById('image') <img id="image" src="">
+        this.img.src = path;
+    }
 
     moveRight(){
         console.log('Moving right');
