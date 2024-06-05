c9c2a2a53ebe47354b01eb602356f7f9fec5b723
TimoRenken <timorenken@outlook.de>
2024-06-05 09:47:42 +0200

    add animationFrame to draw()

--- models/world.class.js
+++ models/world.class.js
@@ -5,14 +5,22 @@ class World{
         new Chicken(),
         new Chicken(),
     ];
+    canvas;
     ctx;
 
     constructor(canvas){
        this.ctx = canvas.getContext('2d');
+       this.canvas = canvas;
        this.draw();
     }
 
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(this.charakter.img, this.charakter.x, this.charakter.y, this.charakter.width , this.charakter.heigt)
+
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        })
     }
 }
