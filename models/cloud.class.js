45d1b8c648e7fd7b3fae1cf665958bfaa4c750a1
TimoRenken <timorenken@outlook.de>
2024-06-08 14:08:05 +0200

    animate clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -4,10 +4,19 @@ class Cloud extends MovableObject {
     heigt = 250;
 
 
-    constructor(){
+    constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
-    
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15; // decreased "x" 0.15px at 60fps
+            if (this.x < -450) {
+                this.x = 640; 
+            }
+        }, 1000 / 60);
     }
 }
