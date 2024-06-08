13635353cac35c3a882b5b5f6e4e15449cbca681
TimoRenken <timorenken@outlook.de>
2024-06-08 12:21:28 +0200

    edit objects heigt and width

--- models/character.class.js
+++ models/character.class.js
@@ -1,10 +1,12 @@
 class Character extends MovableObject{
-   
+    
+    heigt = 250
+    y = 180
+
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
 
     jump(){
-
-    }
+    };
 }
