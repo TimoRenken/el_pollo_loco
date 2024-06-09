95b070d1ded9fc3fa71573084cbdaa7dee758763
TimoRenken <timorenken@outlook.de>
2024-06-09 17:12:56 +0200

    add walking animation to pepe

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,14 +1,27 @@
 class MovableObject {
     x = 120
     y = 280
-    img;
     heigt = 150;
     width = 100;
+    img;
+    imageCache = {};
 
     
     loadImage(path){           // loadImage('img/test.png')
         this.img= new Image(); // this.img = document.getElementById('image') <img id="image" src="">
         this.img.src = path;
+    }
+/**
+ * 
+ * @param {arr} array - ['img/2_character_pepe/2_walk/W-21.png,' 'img/2_character_pepe/2_walk/W-22.png, ...]
+ */
+    loadImages(arr){
+       arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = img;
+       });
+        
     }
 
     moveRight(){
