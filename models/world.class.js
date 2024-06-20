7681ea83fcd8a65063843fd51fa686ef1b5e8739
TimoRenken <timorenken@outlook.de>
2024-06-20 10:40:52 +0200

    limit distance to run

--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,6 @@
 class World {
     charakter = new Character();
-    enemies = level1.enemies;
-    clouds = level1.clouds;
-    backgroundObjects = level1.backgroundObjects;
+    level = level1
     canvas;
     ctx;
     keyboard;
@@ -24,10 +22,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
-        this.addObjectsToMap(this.backgroundObjects);
-        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.enemies);
         this.addToMap(this.charakter);
-        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.level.clouds);
         this.ctx.translate(-this.camera_x, 0);
 
         // repeats draw()
