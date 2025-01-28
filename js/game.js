f7c354dfb3b59d6d3c8d03636f5eed50f118a3b2
TimoRenken <timorenken@outlook.de>
2025-01-28 09:00:36 +0100

    game now checks if its muted after restart.

--- js/game.js
+++ js/game.js
@@ -76,9 +76,13 @@ function newGame() {
     world.character.collectedBottles = 0; // Reset the number of collected bottlesd
     initLevel();   // Reinitialize the game
     init();
+    checkMute();
 }
 
 
+/**
+ * This function toggles the sound.
+ */
 function toggleMute() {
     soundIcon = document.getElementById('toggle_sound');
 
@@ -91,6 +95,17 @@ function toggleMute() {
         soundIcon.innerHTML = "volume_up";
         sounds.forEach(sound => sound.muted = false);
         isMuted = false;
+    }
+}
+
+/**
+ * This function checks if the game is muted.
+ */
+function checkMute(){
+    if(isMuted){
+        sounds.forEach(sound => sound.muted = true);
+    } else {
+        sounds.forEach(sound => sound.muted = false);
     }
 }
 
