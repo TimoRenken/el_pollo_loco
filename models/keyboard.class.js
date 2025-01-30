206314b8f9f0288e95a6c47fb27ed90ba45accb1
TimoRenken <timorenken@outlook.de>
2025-01-30 11:08:29 +0100

    Added the ability to use mobile buttons

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -4,9 +4,48 @@ class Keyboard {
     SPACE = false;
     UP = false;
     DOWN = false;
-    D = false;    
+    D = false;
 
-    
+    constructor() {
+        this.buttonTouchStartEvent();
+        this.buttonTouchEndEvent();
+    }
+
+    buttonTouchStartEvent() {
+        window.addEventListener('touchstart', (event) => { // when the user touches the screen
+
+            if (event.target.id == 'btn_right') {  // if the id of the target is equal to the id of the button right      
+                keyboard.RIGHT = true;
+            }
+            if (event.target.id == 'btn_left') {
+                keyboard.LEFT = true;
+            }
+            if (event.target.id == 'btn_jump') {
+                keyboard.SPACE = true;
+            }
+            if (event.target.id == 'btn_throw') {
+                keyboard.D = true;
+            }
+        });
+    }
+
+    buttonTouchEndEvent() {
+        window.addEventListener('touchend', (event) => { // when the user stops touching the screen
+
+            if (event.target.id == 'btn_right') { // if the id of the target is equal to the id of the button right 
+                keyboard.RIGHT = false;
+            }
+            if (event.target.id == 'btn_left') {
+                keyboard.LEFT = false;
+            }
+            if (event.target.id == 'btn_jump') {
+                keyboard.SPACE = false;
+            }
+            if (event.target.id == 'btn_throw') {
+                keyboard.D = false;
+            }
+        });
+    }
 }
 
 window.addEventListener('keydown', (event) => {
